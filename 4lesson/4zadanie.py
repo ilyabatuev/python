21b15a544459f32b33a5c6e16091e7af7cab4cdc
Ilya Batuev <il.batuev@yandex.ru>
2022-02-18 14:13:44 +0500

    4 lesson

---
+++ 4lesson/4zadanie.py
@@ -0,0 +1,12 @@
+import sys
+import utils
+
+if __name__ == "main":
+
+    args = sys.argv[1:]
+
+    for code in args:
+        conv = utils.currency_rates(code)
+        if conv:
+            cur = conv
+            print(f"{cur}")
