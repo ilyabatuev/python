1fc6915d089004b87038164329894cb8e0479d31
Ilya Batuev <il.batuev@yandex.ru>
2022-02-20 20:45:14 +0500

    5 lesson

--- 3zadanie.py
+++ 3zadanie.py
@@ -1,10 +1,24 @@
-def thesaurus(*args):
-    name_dict = {}
-    for name in args:
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей',
+    'Дмитрий', 'Борис', 'Елена'
+]
+groups = [
+    '9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А'
+]
+def gen_of_people():
+    i = 0
+    j = 0
+    while i < len(groups):
+        if i >= len(tutors):
+              yield (None, groups[i])
+              i += 1
+              j += 1
+              break
+        else:
+            yield (tutors[j], groups[i])
+            i += 1
+            j += 1
 
-            if name_dict.get(name[0],):
-                name_dict[name[0]].append(name)
-            else:
-                name_dict[name[0]] = [name]
-    return name_dict
-print(thesaurus("Иван", "Мария", "Петр", "Илья"))
+
+for gen in gen_of_people():
+    print(gen)
--- 5zadanie.py
+++ 5zadanie.py
@@ -1,32 +1,10 @@
-from random import choice
-
-nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
-adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
-adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
-answer = []
-
-def get_jokes(n, flag=False):
-    for i in range(n):
-        random_one = choice(nouns)
-        random_two = choice(adverbs)
-        random_three = choice(adjectives)
-        otvet = f'{random_one} {random_two} {random_three}'
-        answer_two = []
-        print(otvet)
-        if flag == True:
-            answer_two = otvet.split()
-            for nouns_answer in nouns:
-                for i in answer_two:
-                    if nouns_answer == i:
-                        nouns.remove(nouns_answer)
-        for answer_adverbs in adverbs:
-                for i in answer_two:
-                    if answer_adverbs == i:
-                        adverbs.remove(answer_adverbs)
-
-
-        for answer_adjectives in adjectives:
-            for i in answer_two:
-                if answer_adjectives == i:
-                    adjectives.remove(answer_adjectives)
-get_jokes(n=3, flag=True)
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+result = []
+for i in src:
+    n = 0
+    for h in src:
+        if i == h:
+            n += 1
+    if n == 1:
+        result.append(i)
+print(result)
